824373968c8dc7fc7a98b1107b1a14d902a17fa2
Ricardo Barbosa <rbrico@gmail.com>
2022-05-06 13:53:07 -0300

    editor using quill package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import Quill from "quill";
+import { useCallback, useRef } from "react";
+import "quill/dist/quill.snow.css";
+
 function App() {
+  const quillEditorInstance = useRef<Quill>();
+
+  const setQuillEditorRef = useCallback((reactElement: HTMLDivElement) => {
+    if (!reactElement) {
+      return;
+    }
+
+    const editorInstance = new Quill(
+      reactElement,
+      {
+        modules: {},
+        theme: "snow"  
+      }
+    );
+
+    quillEditorInstance.current= editorInstance
+  }, []);
   return (
     <div className="App App-header">
-      
+      <div ref={setQuillEditorRef} />
     </div>
   );
 }
