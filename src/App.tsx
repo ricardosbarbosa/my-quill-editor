fa07dd9899269a883b940b2fe22b052737ad370a
Ricardo Barbosa <rbrico@gmail.com>
2022-05-08 22:05:06 -0300

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import Quill from "quill";
 import { useCallback, useRef } from "react";
 import "quill/dist/quill.snow.css";
 
+import QuillBetterTable from 'quill-better-table'
+
+// Quill.register({
+//   'modules/better-table': QuillBetterTable
+// }, true)
+
 function App() {
   const quillEditorInstance = useRef<Quill>();
 
@@ -13,8 +19,68 @@ function App() {
     const editorInstance = new Quill(
       reactElement,
       {
-        modules: {},
-        theme: "snow"  
+        // debug: 'info',
+        modules: {
+          // toolbar: '#toolbar'
+          toolbar: {
+            container: [["bold"], ["table"]],
+            handlers: {
+              table(enabled:boolean) {
+                
+                if (enabled) {
+                  console.log("ok")
+                  // @ts-ignore
+                  // const tableModule = this.quill.getModule("better-table");
+                  const tableModule = this.quill.getModule('table');
+                  tableModule.insertTable(3, 3);
+                }
+              }
+            }
+          },
+          // keyboard: {
+            // bindings: QuillBetterTable.keyboardBindings
+          // },
+          table: true,
+          // 'better-table': {
+          //   operationMenu: {
+          //     insertColumnRight: {
+          //       text: "insertColumnRight"
+          //     },
+          //     insertColumnLeft: {
+          //       text: "insertColumnLeft"
+          //     },
+          //     insertRowUp: {
+          //       text: "insertRowUp"
+          //     },
+          //     insertRowDown: {
+          //       text: "insertRowDown"
+          //     },
+          //     mergeCells: {
+          //       text: "mergeCells"
+          //     },
+          //     unmergeCells: {
+          //       text: "unmergeCells"
+          //     },
+          //     deleteColumn: {
+          //       text: "deleteColumn"
+          //     },
+          //     deleteRow: {
+          //       text: "deleteRow"
+          //     },
+          //     deleteTable: {
+          //       text: "deleteTable"
+          //     },
+          //     items: {
+          //       unmergeCells: {
+          //         text: 'Another unmerge cells name'
+          //       }
+          //     }
+          //   },
+          // },
+        },
+        placeholder: 'Compose an epic...',
+        readOnly: false,
+        theme: 'snow'
       }
     );
 
